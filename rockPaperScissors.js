b1a75f6be86a0933f154c0b1f15a34e50df901c2
Jiggy Brillantes <brillantes.git@gmail.com>
2021-04-25 15:56:29 +0800

    Added UI

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,90 +1,52 @@
 let player;
-let play;
 let computer;
-
 let playerCounter = 0;
 let computerCounter = 0;
 
-function playerFunction() {
-    let input;
-    do {
-        input = prompt('Rock Paper Scissors?')
-        input = input.toLowerCase();
-        console.log(input);
-    } 
-    while (input !== 'rock' && input !== 'paper' && input !== 'scissors');
-    if (input === 'rock') {
-        return player = 0;
-    }
-    else if (input === 'paper') {
-        return player = 1;
-    }
-    return player = 2;
+const arr = ['Rock', 'Paper', 'Scissors'];
+
+let input = document.querySelectorAll('button');
+input.forEach(button => { 
+    button.addEventListener('click', () => {
+        player = button.id;
+        playRound(player, computer);
+        if (playerCounter === 5) {
+            alert('Player wins the game!');
+            playerCounter = 0;
+            computerCounter = 0;
+            para.textContent = `Player: ${playerCounter} Computer: ${computerCounter}`;
+        }
+        else if (computerCounter === 5){
+            alert('Computer wins the game!');
+            playerCounter = 0;
+            computerCounter = 0;
+            para.textContent = `Player: ${playerCounter} Computer: ${computerCounter}`;
+        }
+        console.log(playerCounter, computerCounter);
+    });
+});
+
+function computerSelection() {
+    return arr[Math.floor(Math.random() * arr.length)];
 }
 
-function computerFunction(){
-        let randomNumber =  Math.floor(Math.random() * 3);
-        return computer = randomNumber;
-}
-
-function playRound(player, computer) {
-    if (player === computer) {
-        alert(`Draw! Player: ${playerCounter} Computer: ${computerCounter}`);
-        return play = 'Draw';
-    }
-    else if (player === 0) {
-        if (computer === 1) {    
-            computerCounter++;
-            alert(`You lose! Player: ${playerCounter} Computer: ${computerCounter}`);       
-            return play = 1;
-        }
-        else if (computer === 2) {  
-            playerCounter++;
-            alert(`You win! Player: ${playerCounter} Computer: ${computerCounter}`);         
-            return play = 0;
-        }
-    }
-    else if (player === 1) {
-        if (computer === 0) {
-            playerCounter++;  
-            alert(`You win! Player: ${playerCounter} Computer: ${computerCounter}`);          
-            return play = 0;
-        }
-        else if (computer === 2) {
-            computerCounter++;     
-            alert(`You lose! Player: ${playerCounter} Computer: ${computerCounter}`);   
-            return play = 1;
-        }
-    }
-    else if (player === 2) {
-        if (computer === 0) {
-            computerCounter++;
-            alert(`You lose! Player: ${playerCounter} Computer: ${computerCounter}`);
-            return play = 1;
-        }
-        else if (computer === 1) {
-            playerCounter++;
-            alert(`You win! Player: ${playerCounter} Computer: ${computerCounter}`);
-            return play = 0;
-        }
-    }
-}
-
-function game() {
-    do {
-        playerFunction();
-        computerFunction();
-        playRound(player,computer);
-        console.log(player, computer, play);
-    } while(playerCounter !== 5 && computerCounter !== 5);
-    if (playerCounter > computerCounter) {
-        alert('Player wins!');
-    }
-    else if (playerCounter < computerCounter) {
-    alert('Computer wins!');
-    }
+function playRound (player, computer) {
+    computer = computerSelection().toLowerCase();
+    if (player === computer) alert('Draw!');
+    else if ((player === 'rock' && computer === 'scissors') || 
+            (player === 'paper' && computer === 'rock') || 
+            (player === 'scissors' && computer === 'paper')) {
+                playerCounter++;
+                alert(`${player.toUpperCase()} beats ${computer.toUpperCase()}. Player wins!`);
+                para.textContent = `Player: ${playerCounter} Computer: ${computerCounter}`;
+            }
     else {
-        alert('Draw');
+        computerCounter++;
+        alert(`${computer.toUpperCase()} beats ${player.toUpperCase()}. Computer wins!`);
+        para.textContent = `Player: ${playerCounter} Computer: ${computerCounter}`;
     }
 }
-game();
+
+const div = document.querySelector('.select');
+const para = document.createElement('p');
+div.appendChild(para);
